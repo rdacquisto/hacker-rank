744c03fdcbc2ac3ec2fc3f92e1222984171f7d82
bellmounte <ryan.dacquisto@gmail.com>
2015-12-16 13:42:35 -0800

    Added 2d array solution.

---
+++ 2d-array.js
@@ -0,0 +1,45 @@
+// https://www.hackerrank.com/challenges/2d-array
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var arr = [];
+    for(arr_i = 0; arr_i < 6; arr_i++){
+       arr[arr_i] = readLine().split(' ');
+       arr[arr_i] = arr[arr_i].map(Number);
+    }
+
+    var max = -9 * 7; // Worstcase is an hourglass of nothing but -9s.
+
+    for (var i = 0; i < 4; i++) {
+    	for (var j = 0; j < 4; j++) {
+    		var current =
+    			arr[i][j] + arr[i][j+1] + arr[i][j+2] +
+    			           arr[i+1][j+1] +
+    			arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+    		max = Math.max(max, current);
+    	}
+    }
+
+    console.log(max);
+}
